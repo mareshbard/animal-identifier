08139605446d843927908ebd9338d05529839e35
mareshbard <leticiapereirag59@gmail.com>
2026-08-11 08:51:51 -0300

    add: animals cases

---
+++ AnimalsIdentifier/ML/Animal.swift
@@ -0,0 +1,192 @@
+import Foundation
+
+enum Animal: String, CaseIterable, Equatable, Sendable {
+    // 90 ANIMAIS
+    case antelope
+    case badger
+    case bat
+    case bear
+    case bee
+    case beetle
+    case bison
+    case boar
+    case butterfly
+    case cat
+    case caterpillar
+    case chimpanzee
+    case cockroach
+    case cow
+    case coyote
+    case crab
+    case crow
+    case deer
+    case dog
+    case dolphin
+    case donkey
+    case dragonfly
+    case duck
+    case eagle
+    case elephant
+    case flamingo
+    case fly
+    case fox
+    case goat
+    case goldfish
+    case goose
+    case gorilla
+    case grasshopper
+    case hamster
+    case hare
+    case hedgehog
+    case hippopotamus
+    case hornbill
+    case horse
+    case hummingbird
+    case hyena
+    case jellyfish
+    case kangaroo
+    case koala
+    case ladybugs
+    case leopard
+    case lion
+    case lizard
+    case lobster
+    case mosquito
+    case moth
+    case mouse
+    case octopus
+    case okapi
+    case orangutan
+    case otter
+    case owl
+    case ox
+    case oyster
+    case panda
+    case parrot
+    case pelecaniformes
+    case penguin
+    case pig
+    case pigeon
+    case porcupine
+    case possum
+    case  raccoon
+    case rat
+    case reindeer
+    case  rhinoceros
+    case  sandpiper
+    case  seahorse
+    case  seal
+    case  shark
+    case   sheep
+    case  snake
+    case  sparrow
+    case squid
+    case squirrel
+    case starfish
+    case swan
+    case tiger
+    case turkey
+    case turtle
+    case whale
+    case wolf
+    case wombat
+    case woodpecker
+    case zebra
+    
+    var displayName: String {
+        switch self {
+        case .antelope: "Antelope"
+        case .badger: "Badger"
+        case .bat: "Bat"
+        case .bear: "Bear"
+        case .bee: "Bee"
+        case .beetle: "Beetle"
+        case .bison: "Bison"
+        case .boar: "Boar"
+        case .butterfly: "Butterfly"
+        case .cat: "Cat"
+        case .caterpillar: "Caterpillar"
+        case .chimpanzee: "Chimpanzee"
+        case .cockroach: "Cockroach"
+        case .cow: "Cow"
+        case .coyote: "Coyote"
+        case .crab: "Crab"
+        case .crow: "Crow"
+        case .deer: "Deer"
+        case .dog: "Dog"
+        case .dolphin: "Dolphin"
+        case .donkey: "Donkey"
+        case .dragonfly: "Dragonfly"
+        case .duck: "Duck"
+        case .eagle: "Eagle"
+        case .elephant: "Elephant"
+        case .flamingo: "Flamingo"
+        case .fly: "Fly"
+        case .fox: "Fox"
+        case .goat: "Goat"
+        case .goldfish: "Goldfish"
+        case .goose: "Goose"
+        case .gorilla: "Gorilla"
+        case .grasshopper: "Grasshopper"
+        case .hamster: "Hamster"
+        case .hare: "Hare"
+        case .hedgehog: "Hedgedog"
+        case .hippopotamus: "Hippopotamus"
+        case .hornbill: "Hornbill"
+        case .horse: "Horse"
+        case .hummingbird: "Hummingbird"
+        case .hyena: "Hyena"
+        case .jellyfish: "Jellyfish"
+        case .kangaroo: "Kangaroo"
+        case .koala: "Koala"
+        case .ladybugs: "Ladybugs"
+        case .leopard: "Leopard"
+        case .lion: "Lion"
+        case .lizard: "Lizard"
+        case .lobster: "Lobster"
+        case .mosquito: "Mosquito"
+        case .moth: "Moth"
+        case .mouse: "Mouse"
+        case .octopus: "Octupus"
+        case .okapi: "Okapi"
+        case .orangutan: "Orangutan"
+        case .otter: "Otter"
+        case .owl: "Owl"
+        case .ox: "Ox"
+        case .oyster: "Oyster"
+        case .panda: "Panda"
+        case .parrot: "Parrot"
+        case .pelecaniformes: "Pelicaniformes"
+        case .penguin: "Penguin"
+        case .pig: "Pig"
+        case .pigeon: "Pigeon"
+        case .porcupine: "Porcupine"
+        case .possum: "Possum"
+        case  .raccoon: "Raccoon"
+        case .rat: "Rat"
+        case .reindeer: "Reindeer"
+        case  .rhinoceros: "Rhinoceros"
+        case  .sandpiper: "Sandpiper"
+        case  .seahorse: "Seahorse"
+        case  .seal: "Seal"
+        case  .shark: "Shark"
+        case   .sheep: "Sheep"
+        case  .snake: "Snake"
+        case  .sparrow: "Sparrow"
+        case .squid: "Squid"
+        case .squirrel: "Squirrel"
+        case .starfish: "Starfish"
+        case .swan: "Swan"
+        case .tiger: "Tiger"
+        case .turkey: "Turkey"
+        case .turtle: "Turtle"
+        case .whale: "Whale"
+        case .wolf: "Wolf"
+        case .wombat: "Wombat"
+        case .woodpecker: "Woodpecker"
+        case .zebra: "Zebra"
+        
+        }
+    }
+}
+
